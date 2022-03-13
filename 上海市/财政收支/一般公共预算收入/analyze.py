dc24b1acae8ab34f62bd7525a37a8fd6e8f492b1
Zhou Yu <nap_yu@hotmail.com>
2022-03-13 12:20:36 +0800

    Added 一般公共预算收入.

---
+++ 上海市/财政收支/一般公共预算收入/analyze.py
@@ -0,0 +1,39 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Mar  7 20:09:49 2022
+
+@author: Zhou Yu
+"""
+import logging
+import sys
+import re
+import pandas as pd
+import matplotlib as mpl
+import matplotlib.pyplot as plt
+import matplotlib.font_manager as fm
+from fontTools.ttLib import TTFont
+import numpy as np
+from pathlib import Path
+import io
+import json
+
+from sympy import elliptic_f
+
+module_fn = 'MyStatisticsData.py'
+d = Path().cwd()
+while True:
+    p = d / module_fn
+    if p.exists():
+        break
+    else:
+        if d.parent.name == d.name:
+            raise FileNotFoundError(f"Can NOT find '{module_fn}'")
+        d = d.parent
+sys.path.append(str(d))
+import MyStatisticsData as msd
+
+dfs = msd.load()
+
+
+
+    
