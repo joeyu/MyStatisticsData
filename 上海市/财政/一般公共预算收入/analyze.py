553a38332c8e12d474155b683c0dfa2ade940ee5
Zhou Yu <nap_yu@hotmail.com>
2022-03-20 11:28:00 +0800

    Updated 上海市一般公共预算收入.

--- 上海市/财政/一般公共预算收入/analyze.py
+++ 上海市/财政/一般公共预算收入/analyze.py
@@ -34,13 +34,17 @@ import MyStatisticsData as msd
 
 dfs = msd.load()
 df = dfs
-major_taxes = ['个人所得税', '企业所得税', '增值税', '房产税', '契税', '证券交易印花税']
-df_major_taxes = df['一般公共预算收入', '税收收入'].loc[:,major_taxes]
-df_major_taxes.plot.bar(title = "上海历年主要税收收入", ylabel = "收入（元）")
-# df0, df1 = tuple(msd.load(['./', '../一般公共预算收支']))
-# df1 = df1.drop(df1.columns[-1], axis = 1)
-# cols = pd.MultiIndex.from_arrays([['一般公共预算收入', '一般公共预算收入'], ['税收收入', '非税收入'], ['税收收入', '非税收入']])
-# df1.columns = cols
-# merged_df = df0.reset_index().merge(df1, how="left").set_index('index')
+fig, axes = plt.subplots(2, 1)
+df_total = df[['总计', '一般公共预算收入合计']]
+df_total.plot.bar(ax = axes[0], title = '上海一般公共预算收入', ylabel = "收入（元）")
+
+major_taxes = ['个人所得税', '企业所得税', '增值税', '契税']
+df_major_taxes = df[major_taxes]
+df_major_taxes.plot.bar(ax = axes[1], title = "上海主要税收收入", ylabel = "收入（元）")
+
+#fp_array = Path("").glob('*.csv')
+#for fp in fp_array:
+#    d = pd.read_csv(fp, index_col = [0], header=[0])
+#    d.to_csv(fp.name)
 
     
