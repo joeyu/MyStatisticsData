cfd279e66d77ac3800bb6e17f59b45ada07941b3
Zhou Yu <nap_yu@hotmail.com>
2022-04-01 12:50:30 +0800

    Cleaned code

--- MyStatisticsData.py
+++ MyStatisticsData.py
@@ -61,10 +61,8 @@ def load(data_fp:str = '.'):
             df.index = pd.PeriodIndex([pd.Period(i) for i in df.index])
 
             freqstr = df.index.freqstr
-            if freqstr in df_freq_groups:
-                df_freq_groups[freqstr] = pd.concat([df_freq_groups[freqstr], df])
-            else:
-                df_freq_groups[freqstr] = df
+            df_freq_groups.setdefault(freqstr, pd.DataFrame())
+            df_freq_groups[freqstr] = pd.concat([df_freq_groups[freqstr], df])
 
         df_array.extend([df.sort_index(axis = 0) for df in df_freq_groups.values()])
         
@@ -81,47 +79,6 @@ def load(data_fp:str = '.'):
 #Set font to support Chinese
 mpl.rc('font', family = 'SimHei', size = 12)
 #mpl.rc('font', family = 'Microsoft New Tai Lue')
-
-def plot_twinx(df1, df2):
-    if isinstance(df1, pd.core.series.Series):
-        df1 = pd.DataFrame(df1)
-
-    if isinstance(df2, pd.core.series.Series):
-        df2 = pd.DataFrame(df2)
-
-    fig, axes = plt.subplots(len(df1.columns), 1, sharex = True)
-    if not isinstance(axes, np.ndarray): 
-        axes = [axes]
-    for i in range(len(df1.columns)):
-        ax = axes[i]
-        ax.bar(df1.index.strftime('%Y'), df1.iloc[:,i])
-        ax.set_ylabel(df1.columns[i] + "（元）")
-
-        ax0 = ax.twinx()
-        ax0.plot(df2.index.strftime('%Y'), df2.iloc[:,i], color = "red")
-        ax0.set_ylabel(df2.columns[i])
-
-def plot_with_pct_change(df, title:str = None):
-    if isinstance(df, pd.core.series.Series):
-        df = pd.DataFrame(df)
-
-    pct = df.pct_change() * 100    
-    pct.columns = [s + " Change %" for s in pct]
-    plot_twinx(df, pct)
-
-def data_and_pct_change(data):
-    pct = data.pct_change()
-    pct.name = "Change%"
-    return pd.concat([data, pct], axis = 1)
-
-def excelto_cvs(excel_fp:str, cvs_fp_prefix:str):
-    sheet_df_dicts = pd.read_excel(excel_fp, sheet_name=None)
-
-    garbages = r"[  　]";
-    for name, df in sheet_df_dicts.items():
-        df.columns = [re.sub(garbages, '', s) for s in df]
-        df = df.applymap(lambda x: re.sub(garbages, '', x) if type(x) == str else x)
-        df.to_csv(f"{cvs_fp_prefix}_{name}.csv", index=False)
 
 def read_raw(raw_filepath, unit = 1E8):
     fp = Path(raw_filepath)
@@ -169,7 +126,7 @@ def plot(df, **kwargs):
             ax.text(k, v + 0.2, v)
     
     axes = ax.get_shared_x_axes().get_siblings(ax)
-    if len(axes) == 1 or axes[-1] == ax:
+    if axes[-1] == ax:
         ax.legend()
     
     return ax
