c4a0ced89a50338ac22f4878d877b2ee32ddf9ea
Zhou Yu <nap_yu@hotmail.com>
2022-04-10 19:49:15 +0800

    Updated covid19.

--- covid19-22/covid19-gz.py
+++ covid19-22/covid19-gz.py
@@ -47,62 +47,56 @@ df = msd.load()
 ser_new_cases = df['广州市'].dropna().astype('int64')
 new_new_cases = {}
 
-new_cases = {}
-profile = webdriver.FirefoxProfile()
-profile.set_preference("network.proxy.type", 0)
-profile.update_preferences()
-with webdriver.Firefox(executable_path='geckodriver', firefox_profile=profile) as driver:
-    driver.get("http://wjw.gz.gov.cn/ztzl/xxfyyqfk/yqtb/")
+def crawl(ser_new_cases):
+    new_cases = {}
+    profile = webdriver.FirefoxProfile()
+    profile.set_preference("network.proxy.type", 0)
+    profile.update_preferences()
+    with webdriver.Firefox(executable_path='geckodriver', firefox_profile=profile) as driver:
+        driver.get("http://wjw.gz.gov.cn/ztzl/xxfyyqfk/yqtb/")
 
-    ul_list_xpath = "/html/body/div/div[3]/div[3]/div[2]/ul"
-    WebDriverWait(driver, 30).until(EC.presence_of_element_located((By.XPATH, ul_list_xpath)))
-    li_list = driver.find_element_by_xpath(ul_list_xpath).find_elements_by_tag_name('li')
-    end = False
-    for li in li_list:
-        try:
-            a = li.find_element_by_tag_name('a')
-        except NoSuchElementException:
-            continue
-        if a.get_attribute('title').find('广州市新冠肺炎疫情情况') == -1:
-            continue
-        #dt = li.find_element_by_tag_name('em').text
-        print(li.text)
-        a.click()
-        WebDriverWait(driver, 10).until(EC.number_of_windows_to_be(2))
-        driver.switch_to.window(driver.window_handles[1])
-        div_xpath = '/html/body/div/div[3]/div[3]/div[2]/div[2]'
-        WebDriverWait(driver, 30).until(EC.presence_of_element_located((By.XPATH, div_xpath)))
-        div = driver.find_element_by_xpath(div_xpath)
-        s = div.text
-        #print(s)
-        pat = re.compile(r"2022年(\d{1,2})月(\d{1,2})日.+?全市新增本土确诊病例(\d+)例(?:（其中(\d+)例为此前报告的无症状感染者转确诊）)?.+?本土无症状感染者(\d+)例")
-        m = pat.search(s)
-        print(m.groups())
-        if m:
-            month, day, new_cases1, new_cases2, new_cases3 = (int(x) if x else 0 for x in m.groups())
-        dt = pd.Period(year = 2022, month = month, day = day, freq = 'D')
-        new_cases = {}
-        new_cases[dt] = new_cases.setdefault(dt, 0)
-        new_cases[dt] += new_cases1 - new_cases2 + new_cases3
+        ul_list_xpath = "/html/body/div/div[3]/div[3]/div[2]/ul"
+        WebDriverWait(driver, 30).until(EC.presence_of_element_located((By.XPATH, ul_list_xpath)))
+        li_list = driver.find_element_by_xpath(ul_list_xpath).find_elements_by_tag_name('li')
+        end = False
+        for li in li_list:
+            try:
+                a = li.find_element_by_tag_name('a')
+            except NoSuchElementException:
+                continue
+            if a.get_attribute('title').find('广州市新冠肺炎疫情情况') == -1:
+                continue
+            #dt = li.find_element_by_tag_name('em').text
+            print(li.text)
+            a.click()
+            WebDriverWait(driver, 10).until(EC.number_of_windows_to_be(2))
+            driver.switch_to.window(driver.window_handles[1])
+            div_xpath = '/html/body/div/div[3]/div[3]/div[2]/div[2]'
+            WebDriverWait(driver, 30).until(EC.presence_of_element_located((By.XPATH, div_xpath)))
+            div = driver.find_element_by_xpath(div_xpath)
+            s = div.text
+            #print(s)
+            pat = re.compile(r"2022年(\d{1,2})月(\d{1,2})日.+?全市新增本土确诊病例(\d+)例(?:（其中(\d+)例为此前报告的无症状感染者转确诊）)?.+?本土无症状感染者(\d+)例")
+            m = pat.search(s)
+            print(m.groups())
+            if m:
+                month, day, new_cases1, new_cases2, new_cases3 = (int(x) if x else 0 for x in m.groups())
+            dt = pd.Period(year = 2022, month = month, day = day, freq = 'D')
+            new_cases = {}
+            new_cases[dt] = new_cases.setdefault(dt, 0)
+            new_cases[dt] += new_cases1 - new_cases2 + new_cases3
 
-        if dt in ser_new_cases:
-            break
-        new_new_cases = {**new_new_cases, **new_cases}
-        time.sleep(1)
-        driver.close()
-        driver.switch_to.window(driver.window_handles[0])
+            if dt in ser_new_cases:
+                break
+            new_new_cases = {**new_new_cases, **new_cases}
+            time.sleep(1)
+            driver.close()
+            driver.switch_to.window(driver.window_handles[0])
 
-#new_cases_merged = {**new_cases_merged, **new_cases}
-
-mpl.rc('font', family = 'SimHei', size = 12)
-# plt.rcParams.update({
-#     "text.usetex": True,
-#     "font.family": "sans-serif",
-#     "font.sans-serif": ["Helvetica"]})
-
-ser_new_new_cases = pd.Series(new_new_cases)
-ser_new_cases = pd.concat([ser_new_cases, ser_new_new_cases]).sort_index()
-df = df.drop('广州市', axis = 1).merge(ser_new_cases.to_frame(name = '广州市'), how = 'outer', left_index=True, right_index=True)
+    return pd.concat([ser_new_cases, pd.Series(new_new_cases)]).sort_index()
+# df = df.drop('广州市', axis = 1).merge(ser_new_cases.to_frame(name = '广州市'), how = 'outer', left_index=True, right_index=True)
+#df = pd.concat([df.drop('广州市', axis = 1), ser_new_cases.to_frame(name = '广州市')], axis = 1)
+df = df.combine_first(ser_new_cases.to_frame(name = '广州市'))
 
 ax = ser_new_cases.plot(title = "广州市新冠每日新增病例", marker = 'x', color = 'r', label = "历史病例数")
 for k, v in ser_new_cases.iteritems():
--- covid19-22/covid19-jl.py
+++ covid19-22/covid19-jl.py
@@ -44,54 +44,56 @@ import MyStatisticsData as msd
 # mpl.rc('font', family = 'SimHei', size = 12)
 df = msd.load()
 ser_new_cases = df['吉林省']
-new_new_cases = {}
 
-profile = webdriver.FirefoxProfile()
-profile.set_preference("network.proxy.type", 0)
-profile.update_preferences()
-with webdriver.Firefox(executable_path='geckodriver', firefox_profile=profile) as driver:
-    driver.get("http://www.jl.gov.cn/szfzt/jlzxd/yqtb/index.html")
+def crawl(ser_new_cases):
+    new_new_cases = {}
+    profile = webdriver.FirefoxProfile()
+    profile.set_preference("network.proxy.type", 0)
+    profile.update_preferences()
+    with webdriver.Firefox(executable_path='geckodriver', firefox_profile=profile) as driver:
+        driver.get("http://www.jl.gov.cn/szfzt/jlzxd/yqtb/index.html")
 
-    ul_list_xpath = "/html/body/div[2]/ul"
-    WebDriverWait(driver, 30).until(EC.presence_of_element_located((By.XPATH, ul_list_xpath)))
-    li_list = driver.find_element_by_xpath(ul_list_xpath).find_elements_by_tag_name('li')
-    for li in li_list:
-        if li.find_element_by_tag_name('a').get_attribute('title').find('吉林省卫生健康委关于新型冠状病毒肺炎疫情情况通报') == -1:
-            continue
-        #dt = li.find_element_by_tag_name('em').text
-        li.click()
-        WebDriverWait(driver, 10).until(EC.number_of_windows_to_be(2))
-        driver.switch_to.window(driver.window_handles[1])
-        div_xpath = "/html/body/div[1]/div[3]/div[2]/div"
-        WebDriverWait(driver, 30).until(EC.presence_of_element_located((By.XPATH, div_xpath)))
-        div = driver.find_element_by_xpath(div_xpath)
-        s = div.text
-        print(s)
-        pat = re.compile(r"(\d{1,2})月(\d{1,2})日.+?全省新增本地确诊病例(?:（轻型）)?(\d+)例.+?(?:新增本地)?无症状感染者(\d+)例")
-        match = pat.findall(s)
-        new_cases = {}
-        for m in match:
-            print(m)
-            if len(m) == 4:
-                month, day, new_case1, new_case2 = (int(x) for x in m)
-            elif len(m) == 2:
-                month, day = (int(x) for x in m)
-                new_case1, new_case2 = 0, 0
+        ul_list_xpath = "/html/body/div[2]/ul"
+        WebDriverWait(driver, 30).until(EC.presence_of_element_located((By.XPATH, ul_list_xpath)))
+        li_list = driver.find_element_by_xpath(ul_list_xpath).find_elements_by_tag_name('li')
+        for li in li_list:
+            if li.find_element_by_tag_name('a').get_attribute('title').find('吉林省卫生健康委关于新型冠状病毒肺炎疫情情况通报') == -1:
+                continue
+            #dt = li.find_element_by_tag_name('em').text
+            li.click()
+            WebDriverWait(driver, 10).until(EC.number_of_windows_to_be(2))
+            driver.switch_to.window(driver.window_handles[1])
+            div_xpath = "/html/body/div[1]/div[3]/div[2]/div"
+            WebDriverWait(driver, 30).until(EC.presence_of_element_located((By.XPATH, div_xpath)))
+            div = driver.find_element_by_xpath(div_xpath)
+            s = div.text
+            print(s)
+            pat = re.compile(r"(\d{1,2})月(\d{1,2})日.+?全省新增本地确诊病例(?:（轻型）)?(\d+)例.+?(?:新增本地)?无症状感染者(\d+)例")
+            match = pat.findall(s)
+            new_cases = {}
+            for m in match:
+                print(m)
+                if len(m) == 4:
+                    month, day, new_case1, new_case2 = (int(x) for x in m)
+                elif len(m) == 2:
+                    month, day = (int(x) for x in m)
+                    new_case1, new_case2 = 0, 0
 
-            dt = pd.Period(year = 2022, month = month, day = day, freq = 'D')
-            new_cases[dt] = new_cases.setdefault(dt, 0)
-            new_cases[dt] += new_case1 + new_case2
+                dt = pd.Period(year = 2022, month = month, day = day, freq = 'D')
+                new_cases[dt] = new_cases.setdefault(dt, 0)
+                new_cases[dt] += new_case1 + new_case2
 
-        if dt in ser_new_cases:
-            break
-        new_new_cases = {**new_new_cases, **new_cases}
-        time.sleep(1)
-        driver.close()
-        driver.switch_to.window(driver.window_handles[0])
+            if dt in ser_new_cases:
+                break
+            new_new_cases = {**new_new_cases, **new_cases}
+            time.sleep(1)
+            driver.close()
+            driver.switch_to.window(driver.window_handles[0])
+    
+    return pd.concat([ser_new_cases, pd.Series(new_new_cases)]).sort_index()
 
-ser_new_new_cases = pd.Series(new_new_cases)
-ser_new_cases = pd.concat([ser_new_cases, ser_new_new_cases])
-df = df.drop('吉林省', axis = 1).merge(ser_new_cases.to_frame(name = '吉林省'), how = 'outer', left_index=True, right_index=True)
+# df = df.drop('吉林省', axis = 1).merge(ser_new_cases.to_frame(name = '吉林省'), how = 'outer', left_index=True, right_index=True)
+df = df.combine_first(ser_new_cases.to_frame(name = '吉林省'))
 
 ax = ser_new_cases.plot(title = "吉林省新冠每日新增病例", marker = 'x', color = 'r', label = "历史病例数")
 for k, v in ser_new_cases.iteritems():
