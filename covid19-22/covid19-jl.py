f0febe611cf196afe138a1fe237f684fb56209d3
Zhou Yu <nap_yu@hotmail.com>
2022-04-10 18:09:18 +0800

    Added covid19 guangzhou.

--- covid19-22/covid19-jl.py
+++ covid19-22/covid19-jl.py
@@ -25,55 +25,26 @@ import json
 import matplotlib.dates as mdates
 from datetime import datetime, timedelta
 import time
+import sys
+
+module_fn = 'MyStatisticsData.py'
+d = Path().cwd()
+while True:
+    p = d / module_fn
+    if p.exists():
+        break
+    else:
+        if d.parent.name == d.name:
+            raise FileNotFoundError(f"Can NOT find '{module_fn}'")
+        d = d.parent
+sys.path.append(str(d))
+import MyStatisticsData as msd
 
 
-mpl.rc('font', family = 'SimHei', size = 12)
-
-new_cases_merged = {
-    '2022-03-01': 64,
-     '2022-03-02': 10,
-     '2022-03-03': 16,
-     '2022-03-04': 43,
-     '2022-03-05': 61,
-     '2022-03-06': 66,
-     '2022-03-07': 75,
-     '2022-03-08': 174,
-     '2022-03-09': 344,
-     '2022-03-10': 246,
-     '2022-03-11': 400,
-     '2022-03-12': 2156,
-     '2022-03-13': 1026,
-     '2022-03-14': 4067,
-     '2022-03-15': 1853,
-     '2022-03-16': 1157,
-     '2022-03-17': 2353,
-     '2022-03-18': 2211,
-     '2022-03-19': 1494,
-     '2022-03-20': 2091,
-     '2022-03-21': 2465,
-     '2022-03-22': 2848,
-     '2022-03-23': 2601,
-     '2022-03-24': 2010,
-     '2022-03-25': 2496,
-     '2022-03-26': 2078,
-     '2022-03-27': 1993,
-     '2022-03-28': 1867,
-     '2022-03-29': 2182,
-     '2022-03-30': 2175,
-     '2022-03-31': 2234,
-     '2022-04-01': 2974,
-     '2022-04-02': 4455,
-     '2022-04-03': 3578,
-     '2022-04-04': 2472,
-     '2022-04-05': 2771,
-     '2022-04-06': 2436,
-     '2022-04-07': 2266,
-     '2022-04-08': 954,
-     '2022-04-09': 997,
-}
-
-new_cases_merged = {datetime.strptime(dt, '%Y-%m-%d'): v for dt, v in new_cases_merged.items()}
-new_cases = {}
+# mpl.rc('font', family = 'SimHei', size = 12)
+df = msd.load()
+ser_new_cases = df['吉林省']
+new_new_cases = {}
 
 profile = webdriver.FirefoxProfile()
 profile.set_preference("network.proxy.type", 0)
@@ -84,7 +55,6 @@ with webdriver.Firefox(executable_path='geckodriver', firefox_profile=profile) a
     ul_list_xpath = "/html/body/div[2]/ul"
     WebDriverWait(driver, 30).until(EC.presence_of_element_located((By.XPATH, ul_list_xpath)))
     li_list = driver.find_element_by_xpath(ul_list_xpath).find_elements_by_tag_name('li')
-    end = False
     for li in li_list:
         if li.find_element_by_tag_name('a').get_attribute('title').find('吉林省卫生健康委关于新型冠状病毒肺炎疫情情况通报') == -1:
             continue
@@ -99,6 +69,7 @@ with webdriver.Firefox(executable_path='geckodriver', firefox_profile=profile) a
         print(s)
         pat = re.compile(r"(\d{1,2})月(\d{1,2})日.+?全省新增本地确诊病例(?:（轻型）)?(\d+)例.+?(?:新增本地)?无症状感染者(\d+)例")
         match = pat.findall(s)
+        new_cases = {}
         for m in match:
             print(m)
             if len(m) == 4:
@@ -107,21 +78,21 @@ with webdriver.Firefox(executable_path='geckodriver', firefox_profile=profile) a
                 month, day = (int(x) for x in m)
                 new_case1, new_case2 = 0, 0
 
-            dt = datetime(2022, month, day)
+            dt = pd.Period(year = 2022, month = month, day = day, freq = 'D')
             new_cases[dt] = new_cases.setdefault(dt, 0)
             new_cases[dt] += new_case1 + new_case2
 
-        if dt in new_cases_merged:
+        if dt in ser_new_cases:
             break
+        new_new_cases = {**new_new_cases, **new_cases}
         time.sleep(1)
         driver.close()
         driver.switch_to.window(driver.window_handles[0])
 
+ser_new_new_cases = pd.Series(new_new_cases)
+ser_new_cases = pd.concat([ser_new_cases, ser_new_new_cases])
+df = df.drop('吉林省', axis = 1).merge(ser_new_cases.to_frame(name = '吉林省'), how = 'outer', left_index=True, right_index=True)
 
-
-new_cases_merged = {**new_cases_merged, **new_cases}
-
-ser_new_cases = pd.Series(new_cases_merged)
 ax = ser_new_cases.plot(title = "吉林省新冠每日新增病例", marker = 'x', color = 'r', label = "历史病例数")
 for k, v in ser_new_cases.iteritems():
     offset = 100
--- covid19-22/covid19-sh.py
+++ covid19-22/covid19-sh.py
@@ -27,49 +27,23 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions as EC
 from scipy.optimize import curve_fit
 import time
+import sys
 
-new_cases_merged = {
-    '2022-03-03': 16,
-    '2022-03-04': 19,
-    '2022-03-05': 28,
-    '2022-03-06': 48,
-    '2022-03-07': 55,
-    '2022-03-08': 65,
-    '2022-03-09': 80,
-    '2022-03-10': 75,
-    '2022-03-11': 83,
-    '2022-03-12': 65,
-    '2022-03-13': 167,
-    '2022-03-14': 139,
-    '2022-03-15': 202,
-    '2022-03-16': 158,
-    '2022-03-17': 260,
-    '2022-03-18': 374,
-    '2022-03-19': 503,
-    '2022-03-20': 758,
-    '2022-03-21': 896,
-    '2022-03-22': 981,
-    '2022-03-23': 983,
-    '2022-03-24': 1609,
-    '2022-03-25': 2264,
-    '2022-03-26': 2676,
-    '2022-03-27': 3500,
-    '2022-03-28': 4456,
-    '2022-03-29': 5964,
-    '2022-03-30': 5637,
-    '2022-03-31': 4482,
-    '2022-04-01': 6309,
-    '2022-04-02': 8153,
-    '2022-04-03': 8935,
-    '2022-04-04': 13350,
-    '2022-04-05': 17037,
-    '2022-04-06': 19967,
-    '2022-04-07': 20899,
-    '2022-04-08': 23204,
-    '2022-04-09': 24752,
-}
+module_fn = 'MyStatisticsData.py'
+d = Path().cwd()
+while True:
+    p = d / module_fn
+    if p.exists():
+        break
+    else:
+        if d.parent.name == d.name:
+            raise FileNotFoundError(f"Can NOT find '{module_fn}'")
+        d = d.parent
+sys.path.append(str(d))
+import MyStatisticsData as msd
 
-new_cases_merged = {datetime.strptime(dt, '%Y-%m-%d'): v for dt, v in new_cases_merged.items()}
+df = msd.load()
+ser_new_cases = df['上海市'].dropna().astype('int64')
 
 # new_cases = {}
 # profile = webdriver.FirefoxProfile()
@@ -117,11 +91,10 @@ mpl.rc('font', family = 'SimHei', size = 12)
 #     "font.family": "sans-serif",
 #     "font.sans-serif": ["Helvetica"]})
 
-ser_new_cases = pd.Series(new_cases_merged)
 #ser_new_cases = ser_new_cases[:datetime(2022, 4, 8)]
 ax = ser_new_cases.plot(title = '上海市新冠每日新增病例及趋势', marker = 'd', color = 'r', label = "历史每日新增病例数")
 ax.set_xlabel("t", fontsize = 15)    
-ax.set_ylabel("病例数I", fontsize = 15)    
+ax.set_ylabel("每日新增病例数I", fontsize = 15)    
 n_day = np.arange(len(ser_new_cases))
 def fun(x, a, b, c):
     return a * np.exp(b * x) + c
@@ -129,7 +102,7 @@ popt, pcov = curve_fit(fun, n_day, ser_new_cases)
 a, b, c = popt
 days = len(ser_new_cases) + 7
 ser_new_cases_fit = pd.Series(np.arange(days)).apply(lambda x: int(a * np.exp(b * x) + c))
-ser_new_cases_fit.index = pd.date_range(ser_new_cases.index[0], periods = days, freq='D')
+ser_new_cases_fit.index = pd.period_range(ser_new_cases.index[0], periods = days, freq='D')
 ax = ser_new_cases_fit.plot(ax = ax, linestyle = '--', marker ='o', color = 'b', label = "拟合数及趋势")
 # x_ticklabels = [x.strftime('%m-%d') for x in ser_new_cases_fit.index]
 y0, y1 = ax.get_ylim()
@@ -185,11 +158,11 @@ def fits(new_cases, start):
     exp = np.exp(b)
     return exp
 
-start = datetime(2022, 3, 18)
+start = pd.Period('2022-3-18')
 rate = fits(ser_new_cases, start)
 rate_index = ser_new_cases[start:].index
 ax_twinx = ax.twinx()
-line, = ax_twinx.plot(rate_index, rate, color = 'purple', linestyle = '--', marker = 'x')
+line, = ax_twinx.plot(rate_index.to_timestamp(), rate, color = 'purple', linestyle = '--', marker = 'x')
 ax_twinx.set_ylabel("日增长率", fontsize = 15, color = 'purple')
 # y0, y1 = ax_twinx.get_ylim()
 rate_max = rate.max()
